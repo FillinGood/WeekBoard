40cbac2a89625cd337630a4d3dd55d8d805f6fd8
Tea-Cup <cozyteacup@pm.me>
2023-07-02 04:48:40 +0700

    DateOnlyConverter

---
+++ DateOnlyConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace WeekBoard {
+	public class DateOnlyConverter : IValueConverter {
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+			if (targetType == typeof(DateTime)) {
+				if (value is DateOnly d) return d.ToDateTime();
+				throw new NotSupportedException($"Unable to convert {value} to {targetType}");
+			} else if (targetType == typeof(DateTime?)) {
+				if (value is null) return null!;
+				if (value is DateOnly d) return d.ToDateTime();
+				throw new NotSupportedException($"Unable to convert {value} to {targetType}");
+			} else if (targetType == typeof(DateOnly)) {
+				if (value is DateTime dt) return dt.ToDateOnly();
+				throw new NotSupportedException($"Unable to convert {value} to {targetType}");
+			} else if (targetType == typeof(DateOnly?)) {
+				if (value is null) return null!;
+				if (value is DateTime dt) return dt.ToDateOnly();
+				throw new NotSupportedException($"Unable to convert {value} to {targetType}");
+			} else {
+				throw new NotSupportedException($"Unable to convert {value} to {targetType}");
+			}
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+			return Convert(value, targetType, parameter, culture);
+		}
+	}
+}
---
+++ Extensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WeekBoard {
+	public static class Extensions {
+		public static DateTime ToDateTime(this DateOnly d) {
+			return new DateTime(d.DayNumber * TimeSpan.TicksPerDay);
+		}
+		public static DateOnly ToDateOnly(this DateTime dt) {
+			return new DateOnly(dt.Year, dt.Month, dt.Day);
+		}
+	}
+}
